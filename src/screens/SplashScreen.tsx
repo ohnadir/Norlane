0088a2b0b9df8287169815639c5deffcba6e7971
Nadir Hossain <nadirhossain336@gmail.com>
2024-04-14 11:56:24 +0600

    added spinner into the splash screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable eol-last */
-import { View, Image } from 'react-native';
+import { View, Image, ActivityIndicator } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
 import Logo from '../assets/logo.png';
+
 
 const SplashScreen = (): React.JSX.Element => {
     return (
@@ -18,6 +19,9 @@ const SplashScreen = (): React.JSX.Element => {
         >
             <View  style={tw `m-auto `} >
                 <Image source={Logo} />
+            </View>
+            <View style={tw `absolute mx-auto top-[70%] left-[45%]`}>
+                <ActivityIndicator size="large" color="#ffffff" />
             </View>
         </View>
     );
